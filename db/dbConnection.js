7c1181b859a58ec7fe57d88c40ad12b6675d0bf7
Joseph Homan <jmhoman332@outlook.com>
2022-03-23 22:35:26 -0700

    env

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
 const knexConfig =
-  require("../knexfile")[process.env.NODE_ENV || "development"];
+  require("../knexfile.js")[process.env.NODE_ENV || "development"];
 
 const dbConnection = knex(knexConfig);
 module.exports dbConnection;
